2e93b59ea4c3093fe7b640b7108fe911f9812a0e
Marios <mariolis1000@gmail.com>
2025-09-24 14:46:05 +0300

    Added shelltest.sh

---
+++ Scripts/shelltest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Saving location paths in a variable
+my_location_from=$(pwd)
+my_location_to=/home/marios/Documents/programming/Basht/bashtutorial
+
+echo "$my_location_from" 
+echo "$my_location_to"
+
+
+
