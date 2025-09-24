16820ca1b2d52ca3e1731701a0c9a38467bed8f4
Marios <mariolis1000@gmail.com>
2025-09-24 15:22:22 +0300

    rewrote shelltest.sh to use a subshell

--- Scripts/shelltest.sh
+++ Scripts/shelltest.sh
@@ -2,8 +2,8 @@
 
 ## Saving location paths in a variable
 my_location_from=$(pwd)
-cd ..
-my_location_to=$(pwd)
+#cd ..
+my_location_to=$(cd ..; pwd)
 cd "$my_location_from"
 
 echo "$my_location_from" 
